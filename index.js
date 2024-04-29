331dfc126cac00020a6714c35aa61efed936033f
Ansari Ahsan <ansariahsan67@gmail.com>
2024-04-29 14:28:21 +0530

    feat: Added crud operation with REPL.

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
-const fs = require('fs');
-console.log(fs)
+const repl = require("repl");
+
+const replServer = repl.start("> ");
+
+const people = [];
+
+const createPeople = (name, age, role) => {
+  const person = {
+    name,
+    age,
+    role,
+  };
+  people.push(person);
+};
+
+const getPeople = () => {
+  console.log("people:", people);
+};
+
+const getPersonByNameAndRole = (name, role) => {
+  return people.filter((data) => data.name === name && data.role === role);
+};
+
+const UpdatePerson = (name, newRole) => {
+  const res = people.find((data) => data.name === name);
+  if (res) {
+    res.role = newRole;
+    return res;
+  } else {
+    console.log(`Sorry ${name} not found in our data.`);
+  }
+};
+
+const deletePerson = (name) => {
+  const index = people.findIndex((person) => person.name === name);
+  if (index !== -1) {
+    people.splice(index, 1);
+    console.log(`Deleted ${name} successfully.`);
+    return people;
+  } else {
+    console.log(`Sorry, ${name} not found in our record.`);
+  }
+};
+
+replServer.context.createPeople = createPeople;
+replServer.context.getPeople = getPeople;
+replServer.context.getPersonByNameAndRole = getPersonByNameAndRole;
+replServer.context.updatePerson = UpdatePerson;
+replServer.context.deletePerson = deletePerson;
